7b86a7bf6bfbb68273f82a9287d467f20be125bc
one1color <koki.111.sui@gmail.com>
2020-11-06 12:08:53 +0100

    Initial Commit

---
+++ swiftUI_tut/swiftUI_tutApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_tutApp.swift
+//  swiftUI_tut
+//
+//  Created by koki isshiki on 06.11.20.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_tutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
