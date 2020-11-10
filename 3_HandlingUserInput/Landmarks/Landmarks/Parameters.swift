36c79c071ab30aebcd5eb3d7bdebc1acb68a2caf
one1color <koki.111.sui@gmail.com>
2020-11-10 17:34:42 +0100

    finished geometory tut and now animation

---
+++ 3_HandlingUserInput/Landmarks/Landmarks/Parameters.swift
@@ -0,0 +1,21 @@
+//
+//  Parameters.swift
+//  Landmarks
+//
+//  Created by koki isshiki on 10.11.20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct Parameters: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Parameters_Previews: PreviewProvider {
+    static var previews: some View {
+        Parameters()
+    }
+}
